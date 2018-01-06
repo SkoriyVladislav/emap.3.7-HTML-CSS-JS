60560dcf1de5d204e7d76016edf070b2e1d8b4fd
Skoriy Vladislav <Skoriy.97@gmail.com>
2018-01-06 06:41:09 +0300

    Add files via upload

--- js/jsScripts.js
+++ js/jsScripts.js
@@ -1,32 +1,36 @@
-$(function() {
-	$(".btn").click(function() {
-		$(".form-signin").toggleClass("form-signin-left");
-    $(".form-signup").toggleClass("form-signup-left");
-    $(".frame").toggleClass("frame-long");
-    $(".signup-inactive").toggleClass("signup-active");
-    $(".signin-active").toggleClass("signin-inactive");
-    $(".forgot").toggleClass("forgot-left");   
-    $(this).removeClass("idle").addClass("active");
-	});
-});
+function validateForm() {
+		var result = true; // результат, используем, чтобы проверять сразу все поля ввода константы
+		var FILL_FIELD = "*Заполните поле",
+			PWD_NOT_EQUAL = "*Значение паролей должно совпадать";
+			
+		// Находим ссылки на элементы сообщений об ошибках
+		var errPwd1 =  document.getElementById("err-pwd1"),
+			errPwd2 =  document.getElementById("err-pwd2");
+			
+		//Предварительно очищаем сообщения об ошибках
+		errPwd1.innerHTML = "";
+		errPwd2.innerHTML = "";
+		
+		// Читаем значения из полей формы
+		var pwd1 = document.forms[1]["pwd1"].value,      // или document.testForm.pwd1.value
+			pwd2 = document.forms[1]["pwd2"].value;      // или document.testForm.pwd2.value
 
-$(function() {
-	$(".btn-signup").click(function() {
-  $(".nav").toggleClass("nav-up");
-  $(".form-signup-left").toggleClass("form-signup-down");
-  $(".success").toggleClass("success-left"); 
-  $(".frame").toggleClass("frame-short");
-	});
-});
-
-$(function() {
-	$(".btn-signin").click(function() {
-  $(".btn-animate").toggleClass("btn-animate-grow");
-  $(".welcome").toggleClass("welcome-left");
-  $(".cover-photo").toggleClass("cover-photo-down");
-  $(".frame").toggleClass("frame-short");
-  $(".profile-photo").toggleClass("profile-photo-down");
-  $(".btn-goback").toggleClass("btn-goback-up");
-  $(".forgot").toggleClass("forgot-fade");
-	});
-});
+		// Проверка паролей
+		if (!pwd1) {
+			errPwd1.innerHTML = FILL_FIELD; 
+			result = false;
+		}  // обязательное заполнение 
+		if (!pwd2) {
+			errPwd2.innerHTML = FILL_FIELD; 
+			result = false;
+		}  // обязательное заполнение
+		if (pwd1 && pwd2 && pwd1 !== pwd2) {
+			errPwd1.innerHTML = PWD_NOT_EQUAL; 
+			errPwd2.innerHTML = PWD_NOT_EQUAL;
+			document.forms[1]["pwd1"].value = "";   // сброс 
+			document.forms[1]["pwd2"].value = "";   // сброс
+			result = false;
+		}   // должны совпадать
+			// возвращаем итог проверки		
+	return result;
+}
